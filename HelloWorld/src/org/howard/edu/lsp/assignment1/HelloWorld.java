5db4a62ad4fad95d2626ae1b5e67d708e72f6f91
greercj96 <greercj96@gmail.com>
2021-01-22 09:41:59 -0400

    Added comment

--- HelloWorld/src/org/howard/edu/lsp/assignment1/HelloWorld.java
+++ HelloWorld/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -5,9 +5,8 @@ package org.howard.edu.lsp.assignment1;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//testcomment
-		System.out.println("Hello World");
+		
+		System.out.println("Hello World"); //print "Hello World"
 	}
 
 }
